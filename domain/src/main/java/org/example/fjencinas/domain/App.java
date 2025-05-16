0ff21b3f69da89af6b16cc4506c71ddcc4fa5841
fjencinas <fjencinasgutierrez@gmail.com>
2025-05-16 09:56:22 +0200

    first commit

---
+++ domain/src/main/java/org/example/fjencinas/domain/App.java
@@ -0,0 +1,13 @@
+package org.example.fjencinas.domain;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
