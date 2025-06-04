150165c2450789fcc1b19c964eb00e10a0b1c258
fjencinas <fjencinasgutierrez@gmail.com>
2025-06-04 12:39:32 +0200

    Enhance ItemMapper with JSON mapping functionality and update Maven compiler version to 17

--- infrastructure/rest-infrastructure/src/main/java/org/example/fjencinas/rest/mapper/ItemMapper.java
+++ infrastructure/rest-infrastructure/src/main/java/org/example/fjencinas/rest/mapper/ItemMapper.java
@@ -1,11 +1,27 @@
 package org.example.fjencinas.rest.mapper;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.example.fjencinas.domain.entity.Item;
 import org.example.fjencinas.rest.openapi.model.ItemRequest;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
+
+import java.util.Map;
 
 @Mapper(componentModel = "spring")
 public interface ItemMapper {
 
+    @Mapping(target = "data", source = "data", qualifiedByName = "mapToJsonNode")
     Item toItem(ItemRequest itemRequest);
+
+    @Named("mapToJsonNode")
+    default JsonNode mapToJsonNode(Map<String, Object> value) {
+        try {
+            return new ObjectMapper().valueToTree(value);
+        } catch (Exception e) {
+            return null;
+        }
+    }
 }
